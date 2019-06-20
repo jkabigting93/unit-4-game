ba86e5a4304619c9c933be36c70583a83440b072
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-06-20 02:00:33 -0400

    finished first draft of pseudocoding

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+// Define variables 
+
+// Pick random target number
+
+// Assign random variables to crystals
+
+// Click listeners on crystals
+
+// On click, add crystal value to current score
+
+// Check if new score exceeds target value
+
+// IF equal, WIN
+
+// ELSE if new score > target value, LOSE
+
+// ELSE if new score < target value, keep playing
+
+// IF win, add to counter
+
+// THEN reset game
+
+// IF loss, add to counter
+
+// THEN reset game
+
+// IF game reset, set score to zero
+
+// THEN randomize crystal values
+
+// THEN randomize target value
